0fabc9edbe09561cfa02aa620e477e577341a234
Jared Tyler <jared@jaredtyler.ca>
2014-04-26 23:28:46 -0400

    First draft (including some testing code) of Timer plugin - adds countdown timers for game start after a quick warmup and enabling PvP after 15 minutes.

---
+++ Timer/src/io/github/jmtyler/minecraft/PvpStartTimer.java
@@ -0,0 +1,63 @@
+package io.github.jmtyler.minecraft;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
+
+import io.github.fourohfour.devcountdown.Countdown;
+import io.github.fourohfour.devcountdown.ServerTime;
+import io.github.fourohfour.devcountdown.Tick;
+
+public class PvpStartTimer extends Countdown
+{
+	protected Plugin plugin;
+
+	protected PvpStartTimer(Plugin plugin)
+	{
+		this.plugin = plugin;
+	}
+
+	public static void run(Plugin plugin)
+	{
+		PvpStartTimer timer = new PvpStartTimer(plugin);
+		timer.run(2 * 60, ServerTime.SECOND, plugin);
+		((Timer)plugin).pvp = timer;
+	}
+
+	protected void onRun(int c, int f)
+	{
+		// Assumes f == ServerTime.SECOND
+		this.plugin.getServer().broadcastMessage("PvP will be allowed in " + String.valueOf(c / 60) + " minutes.");
+
+		CommandSender commandSender = this.plugin.getServer().getConsoleSender();
+		this.plugin.getServer().dispatchCommand(commandSender, "scoreboard teams option Playing friendlyfire false");
+	}
+
+	protected void onCancel()
+	{
+		this.plugin.getServer().broadcastMessage("PvP timer cancelled.");
+	}
+
+	protected void onTick(Tick t)
+	{
+		int secondsLeft = t.getTickID();
+		//this.plugin.getServer().broadcastMessage("tick id: " + String.valueOf(secondsLeft));
+		switch (secondsLeft) {
+			/*case 60 * 10:
+			case 60 * 5:
+				this.plugin.getServer().broadcastMessage(ChatColor.DARK_GREEN + "PvP in " + (secondsLeft / 60) + " minutes.");
+				break;*/
+			case 60:
+				this.plugin.getServer().broadcastMessage(ChatColor.DARK_GREEN + "PvP in ONE minute.");
+				break;
+		}
+	}
+
+	protected void onEnd()
+	{
+		CommandSender commandSender = this.plugin.getServer().getConsoleSender();
+		this.plugin.getServer().dispatchCommand(commandSender, "scoreboard teams option Playing friendlyfire true");
+
+		this.plugin.getServer().broadcastMessage(ChatColor.RED + "PvP NOW ENABLED");
+	}
+}
---
+++ Timer/src/io/github/jmtyler/minecraft/Timer.java
@@ -0,0 +1,38 @@
+package io.github.jmtyler.minecraft;
+
+import io.github.fourohfour.devcountdown.Countdown;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Timer extends JavaPlugin
+{
+	public Countdown pvp;
+
+	public void onEnable()
+	{
+
+	}
+
+	public void onDisable()
+	{
+
+	}
+
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
+	{
+		//getLogger().info("cmd[" + command.getName() + "]: '" + label + "' ... " + StringUtils.join(args));
+		if (command.getName().equalsIgnoreCase("uec")) {
+			if (args.length == 1 && args[0].equals("start")) {
+				GameStartTimer.run(this);
+			} else if (args.length == 1 && args[0].equals("cancel")) {
+				if (this.pvp != null) {
+					this.pvp.cancel();
+				}
+			}
+			return true;
+		}
+		return false;
+	}
+}
