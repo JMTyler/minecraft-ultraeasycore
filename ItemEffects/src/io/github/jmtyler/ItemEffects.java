716902faaa3830a510ac92d93a9423027eb874cb
Jared Tyler <jared@jaredtyler.ca>
2014-04-26 21:13:50 -0400

    First draft of ItemEffects plugin - changes golden apple regen from 2 hearts to 4 hearts.  Will eventually allow effect-customization of any consumable item.

---
+++ ItemEffects/src/io/github/jmtyler/ItemEffects.java
@@ -0,0 +1,54 @@
+package io.github.jmtyler;
+
+import java.util.Map;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class ItemEffects extends JavaPlugin implements Listener
+{
+	protected Map<String, Object> config;
+
+	@Override
+	public void onEnable()
+	{
+		this.getConfig().options().copyDefaults(true);
+		this.saveConfig();
+
+		this.config = this.getConfig().getValues(true);
+
+		this.getServer().getPluginManager().registerEvents(this, this);
+	}
+
+	@Override
+	public void onDisable()
+	{
+
+	}
+
+	@EventHandler
+	public void onPlayerItemConsume(PlayerItemConsumeEvent event)
+	{
+		Player player = event.getPlayer();
+		//Material itemType = event.getItem().getType();
+		//this.getServer().broadcastMessage(player.getName() + " is consuming a " + itemType.name());
+
+		// TODO: Use the config to allow override of ANY consumable item's effect
+		//Material.GOLDEN_APPLE.name()	
+		if (event.getItem().getType().equals(Material.GOLDEN_APPLE)) {
+			//event.setCancelled(true);
+			player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 10 * 20, 1), true);
+		}
+	}
+	
+	public Object getConfig(String path)
+	{
+		return this.config.get(path);
+	}
+}
